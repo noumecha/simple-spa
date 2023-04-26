1a1ceb8ab9c3b2eb8d7e3874fe1518e8ba6d6615
noumecha spaker <noumechaivan@gmail.com>
2023-04-26 05:43:32 +0100

    fist api fecthing

---
+++ src/components/Character.js
@@ -0,0 +1,13 @@
+function Character() {
+    return (
+        <div>
+            <h3>{character.name}</h3>
+            <img src={character.image} alt={character.name} width='300' />
+            <p>
+                {'Origin : ${character.origin && character.origin.name}'}
+            </p>
+        </div>
+    )
+};
+
+export default Character;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom/client'
+// import components
+import List from './components/List'
 
 function App()
 {
-    return <h1>The SPA app</h1>
+    return (
+        <div>
+            <h1>The SPA app</h1>
+            <List/>
+        </div>
+    )
 }
 
 const container = document.getElementById('app')
